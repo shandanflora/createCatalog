59d2ba4bfe693de322190242ca333274f123f697
lily.shan <shandan_flora@hotmail.com>
2021-01-22 15:19:58 +0800

    commit for adding error message and back color

--- mainwindow.py
+++ mainwindow.py
@@ -32,23 +32,41 @@ class main_window(QMainWindow):
     def obj_btn_clicked(self):
         file_name = QFileDialog.getExistingDirectory(self.ui.centralwidget, 'open folder', '/')
         self.ui.edit_obj.setText(file_name)
-        self.ui.statusbar.showMessage("已选择目标路径")
+        self.ui.statusbar.setStyleSheet("0;")
+        self.ui.statusbar.setStyleSheet("font-size:15pt;")
+        if file_name != "":
+            self.ui.statusbar.showMessage("已选择目标路径")
 
     def src_btn_clicked(self):
         file_name = QFileDialog.getOpenFileName(self.ui.centralwidget, 'open file', '/')
         self.ui.edit_src.setText(file_name[0])
         namelist = file_name[0].split('/')
         file = namelist[len(namelist) - 1]
-        new_file = file[0:file.rfind('.')] + "_目录.xlsx"
-        self.ui.edit_file_name.setText(new_file)
-        self.ui.statusbar.showMessage("已选择源文件")
+        if file != "":
+            new_file = file[0:file.rfind('.')] + "_目录.xlsx"
+            self.ui.edit_file_name.setText(new_file)
+            self.ui.statusbar.setStyleSheet("0;")
+            self.ui.statusbar.setStyleSheet("font-size:15pt;")
+            self.ui.statusbar.showMessage("已选择源文件")
 
     def gen_btn_clicked(self):
-        file = self.ui.edit_obj.text() + "/" + self.ui.edit_file_name.text()
-        create_catalog.write_excel(file, create_catalog.read_excel(self.ui.edit_src.text()))
-        self.ui.statusbar.showMessage("已生成目录文件！！！！")
+        if self.ui.edit_src.text() == "":
+            self.ui.statusbar.setStyleSheet("font-size:15pt;""background-color:#FF0000;")
+            self.ui.statusbar.showMessage("源文件不能为空！！！")
+        elif self.ui.edit_obj.text() == "":
+            self.ui.statusbar.setStyleSheet("font-size:15pt;""background-color:#FF0000;")
+            self.ui.statusbar.showMessage("目标路径不能为空！！！")
+        elif self.ui.edit_file_name.text() == "":
+            self.ui.statusbar.setStyleSheet("font-size:15pt;""background-color:#FF0000;")
+            self.ui.statusbar.showMessage("目标文件名不能为空！！！")
+        else:
+            file = self.ui.edit_obj.text() + "/" + self.ui.edit_file_name.text()
+            create_catalog.write_excel(file, create_catalog.read_excel(self.ui.edit_src.text()))
+            self.ui.statusbar.showMessage("已生成目录文件！！！！")
 
     def clear_btn_clicked(self):
+        self.ui.statusbar.setStyleSheet("0;")
+        self.ui.statusbar.setStyleSheet("font-size:15pt;")
         self.ui.statusbar.showMessage("清空编辑框")
         self.ui.edit_src.setText("")
         self.ui.edit_obj.setText("")
